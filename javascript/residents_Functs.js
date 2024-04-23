676b74790fd6c5ec7c291db0b07560d7f30e6783
Diego A <diego0angarita@gmail.com>
2024-04-23 10:24:29 -0500

    cosas innecesarias eliminadas

--- javascript/residents_Functs.js
+++ javascript/residents_Functs.js
@@ -52,18 +52,3 @@ function nombrefiltrado(){
     }
   }
 }
-
-  function crearResidente(){
-    let addName = document.getElementById('addName').value
-    let addAge = document.getElementById('addAge').value
-    let addRoomNumber = document.getElementById('addRoomNumber').value
-    let addAdmissionDate = document.getElementById('addAdmissionDate').value
-    let addHealthCondition = document.getElementById('addHealthCondition').value
-    let addFamiliarName = document.getElementById('addFamiliarName').value
-    let addFamiliarRealtionship = document.getElementById('addFamiliarRelationship').value
-    let addFamiliarPhone = document.getElementById('addFamiliarPhone').value
-
-    let ResidentAdded = new paciente(addName, addAge, addRoomNumber, addAdmissionDate, addHealthCondition, addFamiliarName, addFamiliarRealtionship, addFamiliarPhone)
-    residentsData.push(ResidentAdded) && localStorage.setItem('nuevo paciente', JSON.stringify(ResidentAdded))
-  }
-  
